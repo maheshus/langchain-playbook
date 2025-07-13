d97e2020be2ba808448ddaf1976e428144002415
Mahesh U S <85943462+maheshus@users.noreply.github.com>
2025-07-14 00:32:43 +0530

    LLM pipeline using RunnableSequence
    - Added individual runnables: format_prompt, call_llm, extract_content
    - Composed them into a RunnableSequence pipeline (equivalent to LCEL)
    - Supports structured prompt formatting, model invocation, and output extraction

---
+++ chains/runnables.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnableSequence
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Create a ChatGooleGenerativeAI model
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+# Template with placeholder
+pt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are Hubble, an expert astrophysicist. Your role is to assist users with questions on physics and astrophysics, tailoring explanations to their level of understanding—from beginners to seasoned experts. Provide clear, insightful, and accurate answers to help deepen their knowledge of the universe.",
+        ),
+        ("human", "Hey Hubble, can you help me with——{q}?"),
+    ]
+)
+
+# Create individual runnables (steps in the chain)
+format_prompt = RunnableLambda(lambda x: pt.format_prompt(**x))
+call_llm = RunnableLambda(lambda x: llm.invoke(x.to_messages()))  # type: ignore
+extract_content = RunnableLambda(lambda x: x.content)
+
+# Build chain using RunnableSequence [ equivalent to LangChain Expression Language (LCEL)]
+# Compose the full pipeline: user input → format_prompt → call_llm → extract_content
+# `middle` is a list to allow multiple intermediate steps
+chain = RunnableSequence(first=format_prompt, middle=[call_llm], last=extract_content)
+
+# Instead of writing multiple steps—constructing prompts,
+# invoking the model, parsing the output—every time, a chain lets you bundle them into a single unit
+# You can add logging, memory, agents, tools, or more complex logic without changing your existing structure.
+# Chains make this plug-and-play.
+
+# Run chain with a sample query
+result = chain.invoke({"q": "QZSS comparison to other navigation systems like GPS."})
+print(result)
